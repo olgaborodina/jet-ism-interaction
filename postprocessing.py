Fix units, add gas phase gif

postprocessing.py
@@ -17,7 +17,7 @@ megayear = 3.15576e13
 h = 1#float(0.67)
 unit_velocity = np.float64(100000)
 unit_length = np.float64(3.08568e+18 / h)
-unit_mass = np.float64(1.989e+43 / h)
+unit_mass = np.float64(1.989e+33 / h)
 unit_energy = unit_mass * unit_velocity * unit_velocity
 unit_density = unit_mass / unit_length / unit_length / unit_length 
 unit_time = unit_length / unit_velocity
@@ -28,6 +28,7 @@ mu = np.float64(0.6165) # mean molecular weight
 PROTONMASS = np.float64(1.67262178e-24)
 BOLTZMANN = np.float64(1.38065e-16)
 GRAVITY = np.float64(6.6738e-8) # G in cgs
+rho_to_numdensity = 1. * unit_density / (mu * PROTONMASS) # to cm^-3
 
 has_magnetic_fiels = False
 has_cosmic_rays = False
@@ -38,15 +39,19 @@ def get_time_from_snap(snap_data):
     return float(snap_data["Header"].attrs["Time"])
 
 
-def plot_dens_vel(ax, fn, fac=1.0):
+def plot_dens_vel(ax, fn, fac=1.0, t0=0.0):
     sn = arepo.Simulation(fn)
+
+    part = h5py.File(fn, 'r')
+    numdensity = part['PartType0/Density'][:] * rho_to_numdensity
 
     box = np.array([fac*sn.header.BoxSize,fac*sn.header.BoxSize] )
     center = np.array( [0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize] )
 
-    sn.plot_Aslice("rho", log=True,  axes=ax, box=box, center=center, 
-                   vmin=1e-4, vmax=1e2, cblabel='density')
-    ax.set_title("t=%.2f Myr"%(get_time_from_snap(part) * unit_time_in_megayr))
+    sn.plot_Aslice(numdensity, log=True,  axes=ax, box=box, center=center, vmin=1e-3, vmax=3e4, 
+        cblabel=r'density [cm$^{-3}$]')
+    
+    ax.set_title("t=%.2f Myr"%(get_time_from_snap(part) * unit_time_in_megayr - t0))
 
     #---------
     slicex = sn.get_Aslice(sn.part0.Velocities[:,0], res=25, box = box, center = center)
@@ -60,8 +65,9 @@ def plot_dens_vel(ax, fn, fac=1.0):
     ax.set_xlabel(r'$x$ [pc]')
     ax.set_ylabel(r'$y$ [pc]')
     
+    
      
-def plot_energy_vel(ax, fn, fac=1.0):
+def plot_energy_vel(ax, fn, fac=1.0, t0=0.0):
     sn = arepo.Simulation(fn)
 
     box = np.array([fac*sn.header.BoxSize,fac*sn.header.BoxSize] )
@@ -69,7 +75,7 @@ def plot_energy_vel(ax, fn, fac=1.0):
 
     p = sn.plot_Aslice("u", log=True,  axes=ax, box=box, center=center, cmap='coolwarm',
                       vmin=1e1 , vmax=1e9, cblabel='energy')
-    ax.set_title("t=%.2f Myr"%(get_time_from_snap(part) * unit_time_in_megayr))
+    ax.set_title("t=%.2f Myr"%(get_time_from_snap(part) * unit_time_in_megayr - t0))
 
     #---------
     slicex = sn.get_Aslice(sn.part0.Velocities[:,0], res=25, box = box, center = center)
@@ -83,16 +89,17 @@ def plot_energy_vel(ax, fn, fac=1.0):
     ax.set_xlabel(r'$x$ [pc]')
     ax.set_ylabel(r'$y$ [pc]')
     
-def plot_temp_vel(ax, fn, fac=1.0):
+def plot_temp_vel(ax, fn, fac=1.0, t0=0.0):
     sn = arepo.Simulation(fn)
 
     box = np.array([fac*sn.header.BoxSize,fac*sn.header.BoxSize] )
     center = np.array( [0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize] )
     temp = get_temp(fn, 5/3)
+    part = h5py.File(fn, 'r')
     
     sn.plot_Aslice(temp, log=True,  axes=ax, box=box, center=center, 
-                   vmin=1e3, vmax=1e10, cblabel='temperature', cmap='coolwarm')
-    ax.set_title("t=%.2f Myr"%(get_time_from_snap(part) * unit_time_in_megayr))
+                   vmin=1e3, vmax=1e10, cblabel='temperature [K]', cmap='coolwarm')
+    ax.set_title("t=%.2f Myr"%(get_time_from_snap(part) * unit_time_in_megayr - t0))
 
     #---------
     slicex = sn.get_Aslice(sn.part0.Velocities[:,0], res=25, box = box, center = center)