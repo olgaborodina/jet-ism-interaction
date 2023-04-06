fc89ae94aeb0345ba6f433004e8c4c9165ee0f80
olgaborodina <ol.borodina@icloud.com>
2023-04-06 16:06:56 -0400

    Add jet tracer analysis, more modifications and comments

--- postprocessing.py
+++ postprocessing.py
@@ -13,6 +13,7 @@ from IPython.display import HTML
 
 
 """ units """
+gamma = 5 / 3
 megayear = 3.15576e13
 h = 1#float(0.67)
 unit_velocity = np.float64(100000)
@@ -29,6 +30,7 @@ PROTONMASS = np.float64(1.67262178e-24)
 BOLTZMANN = np.float64(1.38065e-16)
 GRAVITY = np.float64(6.6738e-8) # G in cgs
 rho_to_numdensity = 1. * unit_density / (mu * PROTONMASS) # to cm^-3
+temp_to_u = (BOLTZMANN / PROTONMASS) / mu / (gamma - 1) / unit_velocity / unit_velocity
 
 has_magnetic_fiels = False
 has_cosmic_rays = False
@@ -36,16 +38,31 @@ has_fixed_kinetic_energy = True
 
 
 def get_time_from_snap(snap_data):
+    """
+    Find what time the snapshot has in its header.
+    Input: snap_data (snapshot)
+    Output: time in sim units
+    
+    """
     return float(snap_data["Header"].attrs["Time"])
 
 
 def plot_dens_vel(ax, fn, fac=1.0, t0=0.0):
+    """
+    Plot density distribution in a slice with velocity vectors
+    Input: ax (matplotlib axis where you want to plot), 
+           fn (filename), 
+           fac (factor for a box size; default is 1), 
+           t0 (absolute time of the first snapshot to calculate relative time of the current snapshot)
+    Output: none
+    
+    """
     sn = arepo.Simulation(fn)
 
     part = h5py.File(fn, 'r')
     numdensity = part['PartType0/Density'][:] * rho_to_numdensity
 
-    box = np.array([fac*sn.header.BoxSize,fac*sn.header.BoxSize] )
+    box = np.array([fac * sn.header.BoxSize,fac * sn.header.BoxSize] )
     center = np.array( [0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize] )
 
     sn.plot_Aslice(numdensity, log=True,  axes=ax, box=box, center=center, vmin=1e-3, vmax=3e4, 
@@ -68,11 +85,22 @@ def plot_dens_vel(ax, fn, fac=1.0, t0=0.0):
     
      
 def plot_energy_vel(ax, fn, fac=1.0, t0=0.0):
+    """
+    Plot eternal energy distribution in a slice with velocity vectors
+    Input: ax (matplotlib axis where you want to plot), 
+           fn (filename), 
+           fac (factor for a box size; default is 1), 
+           t0 (absolute time of the first snapshot to calculate relative time of the current snapshot)
+    Output: none
+    
+    """
     sn = arepo.Simulation(fn)
 
     box = np.array([fac*sn.header.BoxSize,fac*sn.header.BoxSize] )
     center = np.array( [0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize] )
-
+    temp = get_temp(fn, 5/3)
+    part = h5py.File(fn, 'r')
+    
     p = sn.plot_Aslice("u", log=True,  axes=ax, box=box, center=center, cmap='coolwarm',
                       vmin=1e1 , vmax=1e9, cblabel='energy')
     ax.set_title("t=%.2f Myr"%(get_time_from_snap(part) * unit_time_in_megayr - t0))
@@ -90,6 +118,15 @@ def plot_energy_vel(ax, fn, fac=1.0, t0=0.0):
     ax.set_ylabel(r'$y$ [pc]')
     
 def plot_temp_vel(ax, fn, fac=1.0, t0=0.0):
+    """
+    Plot temperature distribution in a slice with velocity vectors
+    Input: ax (matplotlib axis where you want to plot), 
+           fn (filename), 
+           fac (factor for a box size; default is 1), 
+           t0 (absolute time of the first snapshot to calculate relative time of the current snapshot)
+    Output: none
+    
+    """    
     sn = arepo.Simulation(fn)
 
     box = np.array([fac*sn.header.BoxSize,fac*sn.header.BoxSize] )
@@ -113,8 +150,97 @@ def plot_temp_vel(ax, fn, fac=1.0, t0=0.0):
     ax.set_xlabel(r'$x$ [pc]')
     ax.set_ylabel(r'$y$ [pc]')
     
+def plot_pressure(ax, fn, fac=1.0, t0=0.0):
+    """
+    Plot pressure distribution in a slice
+    Input: ax (matplotlib axis where you want to plot), 
+           fn (filename), 
+           fac (factor for a box size; default is 1), 
+           t0 (absolute time of the first snapshot to calculate relative time of the current snapshot)
+    Output: none
+    
+    """
+    sn = arepo.Simulation(fn)
+
+    box = np.array([fac*sn.header.BoxSize,fac*sn.header.BoxSize] )
+    center = np.array( [0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize] )
+    temp = get_temp(fn, 5/3)
+    part = h5py.File(fn, 'r')
+    
+    sn.addField("pressure",[1,0,0,0,0,0])
+    
+    T = part['PartType0/InternalEnergy'][:] / temp_to_u
+    n0 = part['PartType0/Density'][:] * rho_to_numdensity 
+    sn['pressure'][:] = (T * n0)
+    sn.plot_Aslice("pressure", log=True, axes=ax, box=box, center=center, 
+                    vmin=1e2, vmax=2e8, cblabel=r'pressure [bar]')
+    ax.set_title("t=%.2f Myr"%(get_time_from_snap(part) * unit_time_in_megayr - t0))
+    ax.set_xlabel(r'$x$ [pc]')
+    ax.set_ylabel(r'$y$ [pc]')    
+    
+def plot_jet_tracer(ax, fn, fac=1.0, t0=0.0):
+    """
+    Plot jet tracer distribution in a slice
+    Input: ax (matplotlib axis where you want to plot), 
+           fn (filename), 
+           fac (factor for a box size; default is 1), 
+           t0 (absolute time of the first snapshot to calculate relative time of the current snapshot)
+    Output: none
+    
+    """
+    sn = arepo.Simulation(fn)
+
+    box = np.array([fac*sn.header.BoxSize,fac*sn.header.BoxSize] )
+    center = np.array( [0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize] )
+    temp = get_temp(fn, 5/3)
+    part = h5py.File(fn, 'r')
+    
+    sn.addField("jet",[1,0,0,0,0,0])
+    
+    x = part['PartType0/Jet_Tracer'][:]
+    x[x<0] = 1e-50
+    sn['jet'][:] = x 
+    sn.plot_Aslice("jet", log=True, axes=ax, box=box, center=center,
+                   vmin=1e-40, vmax=1, cblabel=r'jet tracer')
+    ax.set_title("t=%.2f Myr"%(get_time_from_snap(part) * unit_time_in_megayr - t0))
+    ax.set_xlabel(r'$x$ [pc]')
+    ax.set_ylabel(r'$y$ [pc]')
+    
+def plot_dens_xz(ax, fn, fac=1.0, t0=0.0):
+    """
+    Plot density distribution in a slice in a xz plane
+    Input: ax (matplotlib axis where you want to plot), 
+           fn (filename), 
+           fac (factor for a box size; default is 1), 
+           t0 (absolute time of the first snapshot to calculate relative time of the current snapshot)
+    Output: none
+    
+    """
+    sn = arepo.Simulation(fn)
+
+    part = h5py.File(fn, 'r')
+    numdensity = part['PartType0/Density'][:] * rho_to_numdensity
+
+    box = np.array([fac*sn.header.BoxSize,fac*sn.header.BoxSize] )
+    center = np.array( [0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize, 0.5 * sn.header.BoxSize] )
+
+    sn.plot_Aslice(numdensity, log=True,  axes=ax, box=box, center=center, vmin=1e-3, vmax=3e4, 
+        cblabel=r'density [cm$^{-3}$]', axis=[0, 2])
+    
+    ax.set_title("t=%.2f Myr"%(get_time_from_snap(part) * unit_time_in_megayr - t0))
+    
+    ax.set_xlabel(r'$x$ [pc]')
+    ax.set_ylabel(r'$z$ [pc]')
+    
     
 def get_temp(file, gamma):
+    """
+    Calculate temperature for cells in file
+    Input: file (filename), 
+           gamma (adiabatic index), 
+    Output: temperature in Kelvin
+    
+    """
     part = h5py.File(file, 'r')
     utherm = part['PartType0/InternalEnergy'][:]
     temp_to_u = (BOLTZMANN / PROTONMASS) / mu / (gamma - 1) / unit_velocity / unit_velocity
